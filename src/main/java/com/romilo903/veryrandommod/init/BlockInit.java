0d0fc45358d3fa797bb8e13b6cec7da3e51950e3
Romilo Dev <amiguinhos.rodrigo@gmail.com>
2020-09-19 23:42:36 +0100

    ✨ Add Plastic Block

--- src/main/java/com/romilo903/veryrandommod/init/BlockInit.java
+++ src/main/java/com/romilo903/veryrandommod/init/BlockInit.java
@@ -2,6 +2,11 @@ package com.romilo903.veryrandommod.init;
 
 import com.romilo903.veryrandommod.VeryRandomMod;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,8 +16,22 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @ObjectHolder(VeryRandomMod.MOD_ID)
 @Mod.EventBusSubscriber(modid = VeryRandomMod.MOD_ID, bus = Bus.MOD)
 public class BlockInit {
+    public static final Block plastic_block = null;
+
     @SubscribeEvent
     public static void registerBlocks(final RegistryEvent.Register<Block> event) {
-        
+        event.getRegistry().register(new Block(Block.Properties
+                .create(Material.GLASS)
+                .hardnessAndResistance(0.5f, 15.0f)
+                .sound(SoundType.STEM)
+                .notSolid()
+        ).setRegistryName("plastic_block"));
+    }
+
+    @SubscribeEvent
+    public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(new BlockItem(plastic_block, new Item.Properties()
+                .group(ItemGroup.MATERIALS)
+        ).setRegistryName("plastic_block"));
     }
 }
