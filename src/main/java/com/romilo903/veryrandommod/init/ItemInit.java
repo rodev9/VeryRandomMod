29ea1d19b788c4e30d9790c9f0622ecb6ef8062e
Romilo Dev <amiguinhos.rodrigo@gmail.com>
2020-09-19 18:48:28 +0100

    😃 Add the first item

--- src/main/java/com/romilo903/veryrandommod/init/ItemInit.java
+++ src/main/java/com/romilo903/veryrandommod/init/ItemInit.java
@@ -2,6 +2,7 @@ package com.romilo903.veryrandommod.init;
 
 import com.romilo903.veryrandommod.VeryRandomMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,8 +12,10 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = VeryRandomMod.MOD_ID, bus = Bus.MOD)
 @ObjectHolder(VeryRandomMod.MOD_ID)
 public class ItemInit {
+    public static final Item romilo = null;
 
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("romilo"));
     }
 }
