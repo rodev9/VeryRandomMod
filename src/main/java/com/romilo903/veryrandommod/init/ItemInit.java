1454998f37c8c7c35dfe74b56c753a355eda8ed4
Romilo Dev <amiguinhos.rodrigo@gmail.com>
2020-09-25 23:54:12 +0100

    ✨ Add Nether Star Tools

--- src/main/java/com/romilo903/veryrandommod/init/ItemInit.java
+++ src/main/java/com/romilo903/veryrandommod/init/ItemInit.java
@@ -1,13 +1,16 @@
 package com.romilo903.veryrandommod.init;
 
 import com.romilo903.veryrandommod.VeryRandomMod;
-import net.minecraft.item.Food;
-import net.minecraft.item.Item;
+import com.romilo903.veryrandommod.tools.ModItemTier;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ObjectHolder;
+
+import java.util.function.Supplier;
 
 @Mod.EventBusSubscriber(modid = VeryRandomMod.MOD_ID, bus = Bus.MOD)
 @ObjectHolder(VeryRandomMod.MOD_ID)
@@ -15,6 +18,13 @@ public class ItemInit {
     public static final Item romilo = null;
     public static final Item plastic_fragment = null;
     public static final Item orange = null;
+
+    // Tools
+    public static final Item nether_star_sword = null;
+    public static final Item nether_star_pickaxe = null;
+    public static final Item nether_star_axe = null;
+    public static final Item nether_star_shovel = null;
+    public static final Item nether_star_hoe = null;
 
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Item> event) {
@@ -34,5 +44,31 @@ public class ItemInit {
                         .build()
                 )
         ).setRegistryName("orange"));
+
+        // Tools
+        event.getRegistry().register(new SwordItem(ModItemTier.NETHER_STAR, 4, -2.0F, new Item.Properties()
+                .group(VeryRandomMod.ItemTab)
+                .rarity(Rarity.UNCOMMON)
+        ).setRegistryName("nether_star_sword"));
+
+        event.getRegistry().register(new PickaxeItem(ModItemTier.NETHER_STAR, 1, -2.8F, new Item.Properties()
+                .group(VeryRandomMod.ItemTab)
+                .rarity(Rarity.UNCOMMON)
+        ).setRegistryName("nether_star_pickaxe"));
+
+        event.getRegistry().register(new AxeItem(ModItemTier.NETHER_STAR, 5, -3.3F, new Item.Properties()
+                .group(VeryRandomMod.ItemTab)
+                .rarity(Rarity.UNCOMMON)
+        ).setRegistryName("nether_star_axe"));
+
+        event.getRegistry().register(new ShovelItem(ModItemTier.NETHER_STAR, 0.5F, -3.0F, new Item.Properties()
+                .group(VeryRandomMod.ItemTab)
+                .rarity(Rarity.UNCOMMON)
+        ).setRegistryName("nether_star_shovel"));
+
+        event.getRegistry().register(new HoeItem(ModItemTier.NETHER_STAR, -3, new Item.Properties()
+                .group(VeryRandomMod.ItemTab)
+                .rarity(Rarity.UNCOMMON)
+        ).setRegistryName("nether_star_hoe"));
     }
 }
---
+++ src/main/java/com/romilo903/veryrandommod/tools/ModItemTier.java
@@ -0,0 +1,59 @@
+package com.romilo903.veryrandommod.tools;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+    NETHER_STAR(5, 4013, 15.0F, 8.5F, 30, () -> {
+        return Ingredient.fromItems(Items.NETHER_STAR);
+    });
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.get();
+    }
+}
